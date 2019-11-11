4907cd86c41ddf6734008939d36863df97a74f3a
pd1085 <pd1085@wildcats.unh.edu>
2019-11-11 00:27:50 -0500

    adding student model to student_model.py

---
+++ nhsee/models/student_model.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import Model
+class studet(Model):
+        id=models.CharField(max_length=100 , primary_key=True,blank=False, unique=True)
+        firstname=models.CharField(max_length=100)
+        lastname=models.CharField(max_length=100)
+        school=models.CharField(max_length=100)
+        project_id=models.ForeignKey(project, on_delete=models.CASCADE)
+
+        def __str__(self):
+            return self.id,self.firstname,self.lastname,self.school,self.project_id
