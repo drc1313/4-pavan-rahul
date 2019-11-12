2320d9ca68383c489b74dbb4e9e06d264c467f24
pd1085 <pd1085@wildcats.unh.edu>
2019-11-12 08:08:04 -0500

    modifications in project_view.py file to list the projects

--- nhsee/views/project_views.py
+++ nhsee/views/project_views.py
@@ -12,6 +12,32 @@ import xlrd
 
 
 def projectslisting(request):
+
+
+
+    if 'createprojects' in request.POST:
+       file_path = request.POST.get('filepath')
+       projectsdata=xlrd.open_workbook(file_path)
+       sheet = projectsdata.sheet_by_index(0)
+       names=sheet.cell_value(0,0)
+
+       for projectnum in range(1,sheet.nrows):
+            individualproject=sheet.row_values(projectnum)
+            projectinsert = project(project_id=individualproject[2],project_title=individualproject[1],project_category=individualproject[0],description=individualproject[3])
+            projectinsert.save()
+    if 'projectjudges' in request.POST:
+            projectid = request.POST.get('project_id')
+            judgesq = judgeassignment.objects.filter(project_id=projectid)
+
+
+
+            judge_list=[]
+            for judges in judgesq:
+                judgestatus=judge.objects.filter(judge_id=judges.judge_id)
+                for judgedetails in judgestatus:
+                    judge_list.append({"judge_name":str(judgedetails.fname)+" "+str(judgedetails.lname),"judge_id":judges.judge_id,"project_id":project_id,"judgestatus":True})
+
+            return render(request,'projects_template/assignjudge.html',{"judgesjson":judge_list})
     userl = project.objects.all()
     projectlist=[]
 
@@ -21,7 +47,7 @@ def projectslisting(request):
         description=users.description
         project_title=users.project_title
         project_category=users.project_category
-        projectidlist=judgeassignment.objects.filter(project_id=project_id)
+        projectidlist=judgeassignment.objects.filter(project_id_id=project_id)
         projectcount=[]
 
         for individualpoject in projectidlist:
@@ -32,22 +58,9 @@ def projectslisting(request):
 
         else:
             projectlist.append({"project_id":project_id,"project_title":project_title,"project_category":project_category,"description":description,"projectfilled":True})
-    paginator_projects = Paginator(projectlist, 10) # Show 25 contacts per page
-    print(paginator_projects)
-
+    paginator_projects = Paginator(projectlist, 10)
     page = request.GET.get('page')
     contacts = paginator_projects.get_page(page)
 
-    if 'createprojects' in request.POST:
-       file_path = request.POST.get('filepath')
-       projectsdata=xlrd.open_workbook(file_path)
-       sheet = projectsdata.sheet_by_index(0)
-       projectnames=sheet.cell_value(0,0)
 
-       for projectnum in range(1,sheet.nrows):
-            individualproject=sheet.row_values(projectnum)
-            projectinsert = project(project_id=individualproject[2],project_title=individualproject[1],project_category=individualproject[0],description=individualproject[3])
-            projectinsert.save()
-
-
-    return render(request,'swapprojectassigning.html',{"projectjson":contacts})
+    return render(request,'projects_template/listprojects.html',{"projectjson":contacts})
