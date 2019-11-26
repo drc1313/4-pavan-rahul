00d56444f27690dd7663f9e8a19f7011a06e96d1
pd1085 <pd1085@wildcats.unh.edu>
2019-11-25 19:17:19 -0500

    adding avg calculationa nd rank for projects based on raw score

--- nhsee/views/project_views.py
+++ nhsee/views/project_views.py
@@ -8,19 +8,20 @@ from nhsee import models
 import uuid
 from uuid import UUID
 import os
-import xlrd 
+import xlrd
+from django.db import IntegrityError
 
 
 def projectslisting(request):
 
 
-    
+
     if 'createprojects' in request.POST:
        file_path = request.POST.get('filepath')
        judgesdata=xlrd.open_workbook(file_path)
        sheet = judgesdata.sheet_by_index(0)
        judgenames=sheet.cell_value(0,0)
-    
+
        for judgenum in range(1,sheet.nrows):
             individualjudge=sheet.row_values(judgenum)
             projectinsert = project(project_id=individualjudge[2],project_title=individualjudge[1],project_category=individualjudge[0],description=individualjudge[3])
@@ -28,9 +29,9 @@ def projectslisting(request):
     if 'projectjudges' in request.POST:
             projectid = request.POST.get('project_id')
             judgesq = judgeassignment.objects.filter(project_id=projectid)
-            
 
-            
+
+
             judge_list=[]
             for judges in judgesq:
                 judgestatus=judge.objects.filter(judge_id=judges.judge_id)
@@ -38,6 +39,15 @@ def projectslisting(request):
                     judge_list.append({"judge_name":str(judgedetails.fname)+" "+str(judgedetails.lname),"judge_id":judges.judge_id,"project_id":project_id,"judgestatus":True})
 
             return render(request,'projects_template/assignjudge.html',{"judgesjson":judge_list})
+
+    if 'deleteprojects' in request.GET:
+            deleterequest=request.GET.get('delete')
+            if deleterequest=="deleteprojects":
+                try:
+                    project.objects.all().delete()
+
+                except IntegrityError as e:
+                    pass
     userl = project.objects.all()
     projectlist=[]
 
@@ -49,29 +59,22 @@ def projectslisting(request):
         project_category=users.project_category
         projectidlist=judgeassignment.objects.filter(project_id_id=project_id)
         projectcount=[]
-        totalscores_list=[]
         average_scorelist=[]
         for individualpoject in projectidlist:
             projectcount.append(individualpoject)
             projectid=individualpoject.project_id_id
             judgeid=individualpoject.judge_id_id
-            goalscore=individualpoject.goal_score
-            planscore=individualpoject.plan_score
-            actionscore=individualpoject.action_score
-            resultanalysisscore=individualpoject.result_analysis_score
-            communicationscore=individualpoject.communication_score
             rawscore=individualpoject.raw_score
-            if goalscore or planscore or actionscore or resultanalysisscore or communicationscore  == None:
-                totalscore=0
-            else:
-                totalscore=goalscore+planscore+actionscore+resultanalysisscore+communicationscore
-            average_scorelist.append(totalscore/5)
-            totalscores_list.append(totalscore)
-        project_avg_score=sum(average_scorelist)    
-        project_score=sum(totalscores_list)
-        print(project_score)    
-        
-        n=1
+            average_scorelist.append(rawscore)
+        project_score=sum(average_scorelist)
+        if len(average_scorelist) == 0:
+            project_avg_score=0
+        else:
+            project_avg_score=project_score/len(average_scorelist)
+
+
+
+
 
 
         if len(projectcount) <= 5:
@@ -79,17 +82,15 @@ def projectslisting(request):
 
         else:
             projectlist.append({"project_id":project_id,"project_title":project_title,"project_category":project_category,"description":description,"projectfilled":True,"total_score":project_score,"average_score":project_avg_score})
-    projectranking=sorted(projectlist, key = lambda i: i['total_score'])
-    print(projectranking)
-    #originalranking=projectranking.reverse()
+    projectranking=sorted(projectlist, key = lambda i: i['average_score'])
     originalranking = [ele for ele in reversed(projectranking)]
-    print(originalranking)
+    n=1
     for assignrank in originalranking:
             assignrank["rank"]=n
             n=n+1
-    paginator_projects = Paginator(originalranking, 10) 
+    paginator_projects = Paginator(originalranking, 10)
     page = request.GET.get('page')
     contacts = paginator_projects.get_page(page)
-        
+
 
     return render(request,'projects_template/listprojects.html',{"projectjson":contacts})
